778151aedc13834be1f59c9cc13bda99d59932cb
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-01-20 12:45:19 +0500

    Added multi-line comments

--- comments.py
+++ comments.py
@@ -5,3 +5,10 @@ print("Hello, World!") #This is a comment
 
 #print("Hello, World!")
 print("Cheers, Mate!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
