0c71e91887652ccfaccfdefdabf8fc63ffa2a7ba
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-01-21 10:26:12 +0500

    Variables of numeric types (int, float, complex)

---
+++ numbers.py
@@ -0,0 +1,47 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Integers (whole number, positive or negative, without decimals, of unlimited length)
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Floats (number, positive or negative, containing one or more decimals)
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Also floats with an "e" to indicate the power of 10
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Complex (written with a "j" as the imaginary part)
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
