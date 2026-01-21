c5065c91ab7edf950e30c0fc89e718e0de1aaa86
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-01-21 10:08:57 +0500

    Worked with assigning variables and unpacking collection

--- variables.py
+++ variables.py
@@ -5,8 +5,30 @@ myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
 
-myVariableName = "John"
+myVariableName = "John" #camelcase
 
-MyVariableName = "John"
+MyVariableName = "John" #pascalcase
 
-my_variable_name = "John"
+my_variable_name = "John" #snakecase
+
+
+#one value to many variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#many values to multiple variables
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#unpacking a collection
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
