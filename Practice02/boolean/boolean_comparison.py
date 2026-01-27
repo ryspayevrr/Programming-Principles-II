c958451d4e014de036635826a70cdcb662e23ac3
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-01-27 11:36:35 +0500

    Boolean comparison operators

---
+++ Practice02/boolean/boolean_comparison.py
@@ -0,0 +1,16 @@
+x = 10
+y = 2.6
+z = "Hello"
+a = 2.6
+b = "hello"
+c = "Hello"
+
+print(z == c)
+print(z != b)
+print(x > y)
+print(x < y)
+print(a >= x)
+print(a <= y)
+
+print(8 < x < 12)
+print(b != c and a == y)
