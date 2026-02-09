ce8409fa74025ec6d477fe4d85ccd399ef8667c6
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-02-09 11:38:08 +0500

    Basic functions, returning values

---
+++ Practice03/functions/basic_functions.py
@@ -0,0 +1,19 @@
+def greeting(name):
+    print("Hello", name)
+
+greeting("Alisher")
+
+
+def calculate_sum(a, b):
+    return a + b
+
+print(calculate_sum(3, 4))
+
+
+
+def celcius_to_kelvin(celcius):
+    print(celcius + 273.15)
+
+celcius = float(input())
+
+celcius_to_kelvin(celcius)
