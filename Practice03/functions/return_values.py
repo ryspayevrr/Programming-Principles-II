1af2a457bbbf592cbe008684c1998beb92647659
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-02-09 12:05:10 +0500

    Return values

---
+++ Practice03/functions/return_values.py
@@ -0,0 +1,17 @@
+def substract(a, b):
+    return a - b 
+
+result = substract(139, 43)
+
+print(result)
+
+
+def passwords():
+    return ["128312874192", "aossdk12312", "akL2Dk21Lspq"]
+
+
+data = passwords()
+
+print(data[2])
+print(data[0])
+
