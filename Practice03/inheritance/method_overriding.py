79d18707668c93953c0d46c06f8ac86402d2307c
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-02-10 11:59:38 +0500

    Parent class methods overriding in child classes

--- Practice03/inheritance/method_overriding.py
+++ Practice03/inheritance/method_overriding.py
@@ -8,4 +8,27 @@ class Student:
     def printinfo(self):
         print(self.fname, self.lname)
         print("Major:", self.major)
-        print("Year": self.year)
+        print("Year:", self.year)
+
+    
+class FirstYear(Student):
+    def __init__(self, fname, lname, major, year):
+        super().__init__(fname, lname, major, year)
+
+    def printinfo(self):
+        print(self.fname, self.lname)
+        print("Your major isssss", self.major)
+        print("And you are first year")
+
+# FirstYear inherited all methods and properties from Student
+# But the method printinfo() in FirstYear overrides method printinfo() in Student
+
+x = Student("LeBron", "James", "Robotics", 2)
+
+y = FirstYear("Pedro", "Gonzalez", "Pedagogics", 3)
+
+x.printinfo()
+
+print("\n")
+
+y.printinfo()
