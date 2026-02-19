b5ad40f7c369b3042c77fe512de54e97fe44a08b
ryspayevrr <ratmir.ryspaew164@gmail.com>
2026-02-19 11:28:58 +0500

    modified

--- Practice04/generators.py
+++ Practice04/generators.py
@@ -42,14 +42,17 @@ for _ in evenNumbers(n):
 
 # 3
 
+
+
 def divisible_by_3_and_4(n):
-    for i in range (n + 1):
-        if i % 3 == 0 and i % 4 == 0:
+    for i in range(n + 1):
+        if i % 12 == 0:
             yield i
 
 n = int(input())
 
-print(*divisible_by_3_and_4(n), sep=", ")
+for num in divisible_by_3_and_4(n):
+    print(num, end=" ")
 
 
 
